1c9057199c84a9e65ce386ff0075cac39cae3eb6
AAndersonSantos <andersonsantos.inc01@gmail.com>
2024-08-27 00:19:43 -0300

    Edit users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,13 @@
-exports.returnUsers = function(req, res){
+exports.returnUsers = function (req, res) {
+  const users = [
+    { id: 1, name: "Anderson-Santos" },
+    { id: 2, name: "Homen-Aranha" },
+    { id: 3, name: "Batman" },
+  ];
 
-    const users = [
-        { id: 1, name: 'Anderson Santos' },
-        { id: 2, name: 'Jhon Doe' },
-        { id: 3, name: 'Batman' }
-    ];
-
-    try {
-        res.status(200).json({ users});
-
-    } catch (error) {
-        res.status(500).send("Houve um erro: " + error);
-        
-    }
-}
+  try {
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).send("Houve um erro: " + error);
+  }
+};
